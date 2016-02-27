400134a92bbe1432f83fac7a3e5e3f33dc0f86fb
Joni M Salmi <josalmi@svm-19.cs.helsinki.fi>
2016-02-27 12:06:37 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var restify = require('restify');
+
+var operators = {
+  '+': function(arg1, arg2) { return arg1 + arg2; },
+  '-': function(arg1, arg2) { return arg1 - arg2; },
+  '*': function(arg1, arg2) { return arg1 * arg2; },
+  '/': function(arg1, arg2) { return arg1 / arg2; },
+}
+
+function respond(req, res, next) {
+  var arg1 = parseFloat(req.query.arg1);
+  var op = req.query.op;
+  var arg2 = parseFloat(req.query.arg2);
+  var operator = operators[op];
+  if (!operator) {
+    res.send(op);
+    return next();
+  }
+  res.send(200, operator(arg1, arg2));
+  next();
+}
+
+var server = restify.createServer();
+server.use(restify.queryParser());
+
+server.get('/', respond);
+
+server.listen(31337, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
